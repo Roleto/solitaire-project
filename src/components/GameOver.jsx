39665966e936aaf800bf322856abcc9aa63599b4
Roland Gulya <mgulyaroland@gmail.com>
2025-01-28 20:09:51 +0100

    changing the game over text

--- src/components/GameOver.jsx
+++ src/components/GameOver.jsx
@@ -2,7 +2,7 @@ export default function GameOver({ winner, onRestart }) {
   return (
     <div id="game-over">
       <h2>Game Over</h2>
-      {winner && <p>{winner} Won!</p>}
+      {winner && <p>{winner} is the winner.Congratulations, you have 3 lines! </p>}
       {!winner && <p>It's a draw!</p>}
       {/* {!winner && <p>It&apos;s a draw!</p>} */}
       <button onClick={onRestart}>Restart</button>
